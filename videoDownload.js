2babb21ae216375bca202ff49ee3700cc881f84e
Mati Ariaudo <98933246+matijariaudo@users.noreply.github.com>
2023-11-19 15:35:24 +1000

    change to watch problems3

--- videoDownload.js
+++ videoDownload.js
@@ -2,6 +2,7 @@ const express = require('express');
 const yt = require("yt-converter");
 const fs=require('fs');
 const path=require('path')
+const fse = require('fs-extra');
 
 const app = express()
 require('dotenv').config()
@@ -18,55 +19,82 @@ function buscar_video(url)
     
 }
 
-function descargar_video(url,newName)
+function descargar_video(url,name,carpeta)
 {
     return new Promise((resolve, reject) => {
         yt.convertAudio({
             url: url,
             itag: 140,
-            directoryDownload: __dirname+"/downloads",
-            title: newName
+            directoryDownload: __dirname+"/downloads/"+carpeta,
+            title:name
             }, (a)=>{
             console.log("Bajando: %",a)
             }, (path)=>{
-            console.log("Se ha bajado",__dirname+"/downloads/"+newName)
-            resolve(__dirname+"/downloads/"+newName)
+            console.log("Se ha bajado")
+            resolve( __dirname+"/downloads/"+carpeta)
         })       
     })
     
 }
 function limpiarTexto(texto) {
-    // Reemplazar caracteres con acentos por sus equivalentes sin acentos
-    const mapaAcentos = {
-      'á': 'a', 'é': 'e', 'í': 'i', 'ó': 'o', 'ú': 'u',
-      'Á': 'A', 'É': 'E', 'Í': 'I', 'Ó': 'O', 'Ú': 'U'
-      // Puedes añadir más caracteres con acento y sus equivalentes aquí
-    };
-  
-    texto = texto.replace(/[áéíóúÁÉÍÓÚ]/g, function(match) {
-      return mapaAcentos[match];
-    });
-  
-    // Eliminar puntos y caracteres que no son letras ni paréntesis
-    texto = texto.replace(/[^a-zA-Z() \s]/g, '');
 
+}
+
+function crearCarpeta(name){
+    // Ruta de la carpeta que deseas crear
+    const nombreCarpeta = './downloads/'+name;
+    // Verificar si la carpeta ya existe
+    if (!fs.existsSync(nombreCarpeta)) {
+    // Crear la carpeta
+    fs.mkdir(nombreCarpeta, (err) => {
+        if (err) {
+        console.error('Error al crear la carpeta:', err);
+        } else {
+        console.log('Carpeta creada exitosamente:', nombreCarpeta);
+        }
+    });
+    } else {
+    console.log('La carpeta ya existe:', nombreCarpeta);
+    }
+}
+function eliminarCarpeta(name) {
+    const nombreCarpeta = './downloads/'+name; // Ruta de la carpeta que deseas eliminar
+    // Elimina la carpeta y su contenido de forma recursiva
+    fse.remove(nombreCarpeta)
+      .then(() => {
+        console.log('Carpeta y su contenido eliminados:', nombreCarpeta);
+      })
+      .catch((err) => {
+        console.error('Error al eliminar la carpeta:', err);
+      });
+}
+function generarCodigoAleatorio(nro=4) {
+    const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let codigo = '';
   
-    return texto;
+    for (let i = 0; i < nro; i++) {
+      const randomIndex = Math.floor(Math.random() * caracteres.length);
+      codigo += caracteres.charAt(randomIndex);
+    }
+    return codigo;
 }
   
-function verArchivos(){
-    fs.readdir('./downloads', (err, archivos) => {
-        if (err) {
-            console.error('Error al leer el directorio:', err);
-            return;
-        }
-        
-        console.log('Archivos en la carpeta:');
-        archivos.forEach((archivo) => {
-            console.log(archivo);
+function verArchivos(name){
+    return new Promise((resolve, reject) => {
+        const directorio='./downloads/'+name;
+        let archivoEncontrado=false;
+        console.log("directorio",directorio)
+        fs.readdir(directorio, (err, archivos) => {
+            if (err) {
+                console.error('Error al leer el directorio:', err);
+                return;
+            }
+            archivos.forEach((archivo) => {
+                console.log("Arch",archivo)
+                if(!archivoEncontrado){resolve(archivo);}
+            });
         });
-        return;
-    });
+    })
 }
 
 
@@ -77,27 +105,24 @@ app.use(express.json())
 app.use(express.static(path.join(__dirname, './public')));
 app.get('/api/:url', async(req, res)=>{
     cod=req.params.url;
+    console.log(req.params.url)
     data=await buscar_video("https://www.youtube.com/watch?v="+cod)
     if(!data){
         return res.status(200).json({"error":"No es un video"})
     }else{
-        console.log(req.params.url)
-        nam=data.title; 
-        const nuevoAudio=await descargar_video("https://www.youtube.com/watch?v="+cod,nam)
-        verArchivos();
-        return res.download(nuevoAudio+".mp3",function(e){
+        const provisorioName=generarCodigoAleatorio();
+        crearCarpeta(provisorioName);
+        console.log(1)
+        const nuevoAudio=await descargar_video("https://www.youtube.com/watch?v="+cod,data.title,provisorioName)
+        console.log(2,nuevoAudio,provisorioName)
+        const nameArchivo=await verArchivos(provisorioName);
+        console.log(3,nameArchivo)
+        return res.download('./downloads/'+provisorioName+'/'+nameArchivo,function(e){
             //Si hay error le aviso al cliente
             if(e){
                 return res.status(500).send('Error al descargar el archivo');
             }            
-            //Si no hay error elimino el archivo
-            fs.unlink(nuevoAudio+".mp3", (unlinkErr) => {
-                if (unlinkErr) {
-                    console.error('Error al eliminar el archivo:', unlinkErr);
-                } else {
-                    console.log('Archivo eliminado correctamente');
-                }
-            });
+            eliminarCarpeta(provisorioName)
         })
     }
 })
@@ -114,6 +139,7 @@ console.clear()
 const port=process.env.PORT|| 3000;
 app.listen(port)
 console.log("Listen ",port)
+eliminarCarpeta('ZFGY')
 }
 
 init()
